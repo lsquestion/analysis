58a47a4c149eee263d99f5e986b90798b827e16e
lsquestion <971852457@qq.com>
2018-03-14 23:12:17 +0800

    continue

--- movielens.py
+++ movielens.py
@@ -7,6 +7,21 @@ ratings=pd.read_table('./pydata/ch02/movielens/ratings.dat',sep='::',header=None
 mnames=['movie_id','title','genres']
 movies=pd.read_table('./pydata/ch02/movielens/movies.dat',sep='::',header=None,names=mnames)
 
-print users[:5]
-print ratings[:5]
-print movies[:5]
+data=pd.merge(pd.merge(ratings,users),movies)
+
+mean_ratings=data.pivot_table('rating',index=['title'],columns='gender',aggfunc='mean')
+
+ratings_by_title=data.groupby('title').size()
+
+active_titles=ratings_by_title.index[ratings_by_title>250]
+
+mean_ratings=mean_ratings.ix[active_titles]
+
+top_female_ratings=mean_ratings.sort_index(by="F",ascending=False)
+mean_ratings['diff']=mean_ratings['M']-mean_ratings['F']
+
+sorted_by_diff=mean_ratings.sort_index(by='diff')
+print sorted_by_diff[:15]
+#print ratings_by_title[:10]
+
+#print mean_ratings[:5]
